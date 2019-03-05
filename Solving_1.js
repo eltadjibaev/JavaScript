8864ce743956815c7fa3d123c8a2dc005ffbecdc
eltadjibaev <eltadjibaev@gmail.com>
2019-03-05 09:34:04 +0000

    Problem Solving Approach

---
+++ Solving_1.js
@@ -0,0 +1,49 @@
+// function charCount(str) {
+//     var obj = {};
+//     for (var i = 0; i < str.length; i++) {
+//         var char = str[i].toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             if (obj[char] > 0) {
+//                 ++obj[char];
+//             } else {
+//                 obj[char] = 1;
+//             }
+//         }
+//     }
+//     return obj;
+// }
+
+
+// console.log(charCount('hello'));
+
+// function charCount(str) {
+//     var obj = {};
+//     for (var char of str) {
+//         char = char.toLowerCase();
+//         if (/[a-z0-9]/.test(char)) {
+//             if (obj[char] > 0) {
+//                 ++obj[char];
+//             } else {
+//                 obj[char] = 1;
+//             }
+//         }
+//     }
+//     return obj;
+// }
+
+
+// console.log(charCount('hello'));
+
+function charCount(str) {
+    var obj = {};
+    for (var char of str) {
+        char = char.toLowerCase();
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] = ++obj[char] || 1;
+        }
+    }
+    return obj;
+}
+
+
+console.log(charCount('hello'));

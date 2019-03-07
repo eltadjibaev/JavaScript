3de5e4d075e44131c38af5fd76707b4536629c6f
eltadjibaev <eltadjibaev@gmail.com>
2019-03-07 06:57:55 +0000

    Anagrams

---
+++ Solving_3.js
@@ -0,0 +1,45 @@
+// function validAnagram(s, t){
+//   if(s.length !== t.length){
+//       return false;
+//   }
+  
+//   var dict = {};
+//   for(let i = 0; i < s.length; i++){
+//       if(!dict[s[i]]){
+//           dict[s[i]] = 1;
+//       } else {
+//           dict[s[i]]++;
+//       }
+//   }
+  
+//   for(let i = 0; i < t.length; i++){
+//       if(!dict[t[i]]){
+//           return false;
+//       }else {
+//           dict[t[i]]--;
+//       }
+//   }
+//   return true;
+// }
+
+
+function validAnagram(s, t){
+  if(s.length !== t.length){
+      return false;
+  }
+  
+  var lookup = {};
+  for(let i = 0; i < s.length; i++){
+      lookup[s[i]] ? lookup[s[i]]++ : lookup[s[i]] = 1;
+  }
+  
+  for(let i = 0; i < t.length; i++){
+      if(!lookup[t[i]]){
+          return false;
+      }else {
+          lookup[t[i]]--;
+      }
+  }
+  return true;
+}
+console.log(validAnagram("anagrams", "anagramm"));

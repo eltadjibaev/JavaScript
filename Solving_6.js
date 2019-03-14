f1d46560c274cdaf093da4e90917fa9ff453cbcf
eltadjibaev <eltadjibaev@gmail.com>
2019-03-14 12:52:28 +0000

    Recursion Problems

--- Solving_6.js
+++ Solving_6.js
@@ -60,9 +60,25 @@
 // // fib(28) // 317811
 // // fib(35) // 9227465
 
-function reverse(str) {
-    if(str.length === 1) { return str; }
-    return reverse(str.slice(1)) + str[0];
+///////////////////////////////////////////
+
+// function reverse(str) {
+//     if(str.length === 1) { return str; }
+//     return reverse(str.slice(1)) + str[0];
+// }
+
+// console.log(reverse('awesome'));
+
+///////////////////////////////////////////////
+
+function isPalindrome(str){
+  if(str.length === 1) { return true; }
+  if(str[0] !== str[str.length-1]) { return false; }
+  return isPalindrome(str.slice(1, -1));
 }
 
-console.log(reverse('awesome'));
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false

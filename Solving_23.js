989307a843452294e406a79ed2a76b91df42ae84
eltadjibaev <eltadjibaev@gmail.com>
2019-05-01 02:32:11 +0000

    Asynchronous JavaScript

---
+++ Solving_23.js
@@ -0,0 +1,16 @@
+//Asynchronous JavaScript
+
+const second = () => {
+     setTimeout(() => {
+         console.log('Async Hey there');
+     }, 2000);
+ }
+
+ const first = () => {
+      console.log('Hey there');
+        second();
+        console.log('The end');
+     }
+
+first();
+
